4546aa98163d2c149eb76578cd22000e83cffc75
kurt-siefken <109696124+kurt-siefken@users.noreply.github.com>
2025-05-05 07:58:27 -0400

    Initial commit

---
+++ logic.js
@@ -0,0 +1,132 @@
+function shuffle(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
+
+
+
+function startwheel() {
+
+	document.getElementById("questionlist").innerHTML = "";
+
+	if (decklist = document.getElementById("questiondeck").value == 'Quick') {
+		shuffle(QuickQuestion);
+		for (let i = 0; i < 3; i++) {
+		QuickQuestion.forEach(creatediv);
+		}	
+	}
+
+	if (decklist = document.getElementById("questiondeck").value == 'Discussion') {
+		shuffle(DiscQuestion);
+		for (let i = 0; i < 3; i++) {
+		DiscQuestion.forEach(creatediv);
+		}
+	}
+
+	if (decklist = document.getElementById("questiondeck").value == 'Alt Rock') {
+		shuffle(AltRockQuestion);
+		for (let i = 0; i < 3; i++) {
+		AltRockQuestion.forEach(creatediv);
+		}
+	}
+
+	if (decklist = document.getElementById("questiondeck").value == 'Ridiculous') {
+		shuffle(RidicQuestion);
+		for (let i = 0; i < 3; i++) {
+		RidicQuestion.forEach(creatediv);
+		}
+	}
+
+	if (decklist = document.getElementById("questiondeck").value == 'Work') {
+		shuffle(WorkQuestion);
+		for (let i = 0; i < 3; i++) {
+		WorkQuestion.forEach(creatediv);
+		}
+	}
+
+	if (decklist = document.getElementById("questiondeck").value == 'Positive') {
+		shuffle(PosQuestion);
+		for (let i = 0; i < 3; i++) {
+		PosQuestion.forEach(creatediv);
+		}
+	}
+
+
+
+
+
+
+
+document.getElementById("questionlist").classList.remove("stopanimation"); 
+document.getElementById("questionlist").classList.add("startanimation");
+document.getElementById("startbutton").classList.add("startbuttonon");
+document.getElementById("stopbutton").classList.remove("stopbuttonon");
+document.getElementById("flipswitchbody").classList.add("flipswitchoff");
+document.getElementById("flipswitchbody").classList.remove("flipswitchon");
+}
+
+
+
+
+
+function creatediv(questionnumber) {
+
+document.getElementById("questionlist").innerHTML += "<div><p>" + questionnumber + "</p></div>";
+
+}
+
+
+
+
+
+
+
+
+function stopwheel() {
+document.getElementById("questionlist").classList.remove("startanimation");
+document.getElementById("questionlist").classList.add("stopanimation");
+document.getElementById("startbutton").classList.remove("startbuttonon");
+document.getElementById("stopbutton").classList.add("stopbuttonon");
+document.getElementById("flipswitchbody").classList.remove("flipswitchoff");
+document.getElementById("flipswitchbody").classList.add("flipswitchon");
+
+}
+
+
+
+
+
+
+window.onload = function() {
+
+
+  const deckElement = document.getElementById("questiondeck");
+
+  deckElement.addEventListener("change", function() {
+    const deckValue = deckElement.value;
+
+
+document.getElementById("pagebody").className = '';
+document.getElementById("pagebody").classList.add(deckValue.replace(/\s/g, "") + "Questions");
+
+
+		document.getElementById("thedeck").innerHTML = deckValue + " Question";
+	document.getElementById("questionlist").innerHTML = "";
+
+
+  });
+
+};
+
+
+
+
+
+
+
+
+
---
+++ question-list.js
@@ -0,0 +1,201 @@
+
+const QuickQuestion = [];
+
+
+/*----*/
+QuickQuestion[0] = "What was your worst injury?";
+QuickQuestion[1] = "What is the best Olympic sport to watch?";
+QuickQuestion[2] = "Where have you been that you really want to return to?";
+QuickQuestion[3] = "What wild animal would you like to keep as a pet?";
+QuickQuestion[4] = "Where is the coolest place you've been on vacation?";
+QuickQuestion[5] = "What is your favorite article of clothing you own?";
+QuickQuestion[6] = "What is your breakfast food of choice?";
+QuickQuestion[6] = "What wild animal would you be terrified to face?";
+QuickQuestion[7] = "What subject could you ace on Jeopardy?";
+QuickQuestion[8] = "What is a place you've always wanted to visit?";
+QuickQuestion[9] = "What is the best way to eat a potato?";
+QuickQuestion[10] = "If you had to move to a new state, where would you move?";
+QuickQuestion[11] = "What is the best smell?";
+QuickQuestion[12] = "What is the last frivolous thing you spent money on?";
+QuickQuestion[13] = "What is the best equipment to play on at a playground?";
+QuickQuestion[14] = "What is your guilty pleasure food?";
+QuickQuestion[15] = "What was the last movie or show you really got into?";
+QuickQuestion[16] = "What store can you spend a lot of time just looking around?";
+QuickQuestion[17] = "What is your favorite non-alcoholic drink?";
+QuickQuestion[18] = "If you had a million dollars to donate to charity, which charity would you choose?";
+QuickQuestion[19] = "What is hanging on your refrigerator door?";
+QuickQuestion[20] = "How many Taylor Swift songs can you name?";
+QuickQuestion[21] = "Fill in the blank: When I dance, I look ___.";
+QuickQuestion[22] = "What food is the most healthy and delicioius?";
+QuickQuestion[23] = "What streaming service do you use most?";
+QuickQuestion[24] = "How far do you live from where you grew up?";
+
+
+
+
+
+
+const DiscQuestion = [];
+
+DiscQuestion[0] = "What is a question you have that Google can't answer?";
+DiscQuestion[1] = "What is an embarrassing situation from your past that you're willing to admit?";
+DiscQuestion[2] = "What is something kind that a stranger has done for you?";
+DiscQuestion[3] = "What is a small part of your life that you really enjoy?";
+DiscQuestion[4] = "If you had to give a half hour talk right now, what could you talk about?";
+DiscQuestion[5] = "What is something you hated as a kid, but love now?";
+DiscQuestion[6] = "What would look really cool if it was made out of legos?";
+DiscQuestion[7] = "What's an annoying pet peeve that you have?";
+DiscQuestion[8] = "What's a quality in someone that makes you respect them instantly?";
+DiscQuestion[9] = "What's a show or movie that, whenever it's on tv, you always stop and watch it?";
+DiscQuestion[10] = "What is a word that you say wrong on purpose because it's funny?";
+DiscQuestion[11] = "What is something from your childhood that kids today won't understand?";
+DiscQuestion[12] = "What is a word or saying local to the area where you live?";
+DiscQuestion[13] = "When you hear <i>a guy walks into a bar</i> jokes, what kind of bar do you picture?";
+DiscQuestion[14] = "What do you think of possums?";
+DiscQuestion[15] = "When you think about school lunch, what do you think of?";
+DiscQuestion[16] = "What is a word you mispronounced for a long time?";
+DiscQuestion[17] = "What is something in popular culture that you just don't get?";
+DiscQuestion[18] = "What is something everyone should experience at least once?";
+DiscQuestion[19] = "What is a recurring dream that you have?";
+DiscQuestion[20] = "What would a perfect vacation look like to you?";
+DiscQuestion[21] = "You have to go on a tv game show. What show do you choose?";
+DiscQuestion[22] = "What's something seemingly simple that blows your mind?";
+DiscQuestion[23] = "When you go on vacation, are you an overplanner, or do you just go with whatever?";
+
+
+
+const RidicQuestion = [];
+
+RidicQuestion[0] = "If you were writing a political thriller, what would the president's name be?";
+RidicQuestion[1] = "If our reality is just a simulation, what is the hypothesis being tested?";
+RidicQuestion[2] = "How many pigeons could you carry for a half mile?";
+RidicQuestion[3] = "If you were stuck in a Walmart for three days, how would you pass the time?";
+RidicQuestion[4] = "What animal would you like to see on rollerskates?";
+RidicQuestion[5] = "If you could run away and join the circus, what circus job would you want?";
+RidicQuestion[6] = "If you had to eat a crayon, what color would you choose?";
+RidicQuestion[7] = "What is a common word that sounds like it could be a swear word?";
+
+
+
+
+
+
+const AltRockQuestion = [];
+
+AltRockQuestion[0] = "What one song best explains the '90s alt rock era?";
+AltRockQuestion[1] = "What bands' shirt(s) did you own? What was your favorite?";
+AltRockQuestion[2] = "What '90s band do you think deserved more respect?";
+AltRockQuestion[3] = "What '90s band had the stupidest band name?";
+AltRockQuestion[4] = "What was your first cd?"
+AltRockQuestion[5] = "What band could you mention that makes people say <i>I totally forgot about them!</i>?"
+AltRockQuestion[6] = "What '90s movie had the best soundtrack?"
+AltRockQuestion[7] = "What '80s band had a huge influence on '90s rock?"
+AltRockQuestion[8] = "You're in a kick-ass dad-rock band! What '90s song are you going to cover?"
+AltRockQuestion[9] = "What '90s alt rock song got you through the hard times?"
+AltRockQuestion[10] = "What '90s alt rock band went on to have the most surprising career?"
+AltRockQuestion[11] = "What is the best sub-genre of '90s alt rock?";
+AltRockQuestion[12] = "Had Kurt Cobain survived, what would the future of Nirvana sound like?";
+AltRockQuestion[13] = "Had Shannon Hoon survived, what would the future of Blind Melon sound like?";
+AltRockQuestion[14] = "What is the best Christmas or holiday song by a '90s band?"
+AltRockQuestion[15] = "What band or artist always had interesting lyrics?"
+AltRockQuestion[16] = "What cover art from the '90s stands out to you?"
+AltRockQuestion[17] = "How would the '90s be different if we had Spotify?"
+AltRockQuestion[18] = "What album from the '90s is the best from start to finish, with no skips?"
+AltRockQuestion[19] = "You can play one song that will unite humanity. What song do you choose?"
+AltRockQuestion[20] = "The '90s swing era: cool or cringe?"
+AltRockQuestion[21] = "What '90s band would you like to see reunite?"
+AltRockQuestion[22] = "What's a <i>terrible</i> song that you can't understand why it became popular?"
+AltRockQuestion[23] = "What's a <i>great</i> song but you can't understand why it became popular?"
+AltRockQuestion[24] = "What was your favorite music video?"
+AltRockQuestion[25] = "What song from the '90s are you embarrassed to admit that you actually like?"
+AltRockQuestion[26] = "What was your first concert?"
+AltRockQuestion[27] = "Who was your '90s alt rock crush?"
+AltRockQuestion[28] = "What recent song would fit right into the '90s alt rock scene?"
+AltRockQuestion[29] = "What '90s alt rock song will always be a banger?"
+AltRockQuestion[30] = "What band would you really like see a behind the scenes documentary about?"
+AltRockQuestion[31] = "What is the saddest song of the '90s?"
+AltRockQuestion[32] = "Spoken word: Cool or not cool?"
+AltRockQuestion[33] = "REM or U2?"
+AltRockQuestion[34] = "What song can you, your parents, and your kids all agree on?"
+AltRockQuestion[35] = "Weezer's blue album cover: cool or awful?"
+AltRockQuestion[36] = "Offspring or Green Day?"
+AltRockQuestion[37] = "What was the best grunge band?"
+AltRockQuestion[38] = "Lollapalooza or Warped Tour?"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const WorkQuestion = [];
+
+WorkQuestion[0] = "What month should have a holiday?"
+WorkQuestion[1] = "If you had to email the CEO an emoji right now, what would it be?"
+WorkQuestion[2] = "Be honest... Have you ever attended a conference call in your pajamas?"
+WorkQuestion[3] = "How has AI impacted your job?"
+WorkQuestion[4] = "What's the most mundane part of your job?"
+WorkQuestion[5] = "What would you do if a co-worker inexplicably showed up to a zoom call in a Spiderman mask?"
+WorkQuestion[6] = "If you work from home, what do you do with your saved commute time?"
+WorkQuestion[7] = "Do you know your company's motto?"
+WorkQuestion[8] = "How do you decompress after a particularly hard day at work?"
+WorkQuestion[9] = "What coworker would you like to swap jobs with for a day?"
+WorkQuestion[10] = "How many cups of coffee do you drink on an average day?"
+WorkQuestion[11] = "How messy is your workspace?"
+WorkQuestion[12] = "How many sticky notes do you have posted around your office?"
+WorkQuestion[13] = "How good are you at social media?"
+WorkQuestion[14] = "What's your best story while traveling for work?"
+WorkQuestion[15] = "What's a skill that's not necessarly work related that has helped you at your job?"
+WorkQuestion[16] = "What is a work skill that you admire in other people?"
+WorkQuestion[17] = "How do you take a break while working from home?"
+WorkQuestion[18] = "What is some office lingo that you find funny?"
+WorkQuestion[19] = "What did you want to be when you grew up, and what do you do now?"
+WorkQuestion[20] = "What would you do if you had a 3-month paid sabbatical?"
+WorkQuestion[21] = "You fall asleep in a work meeting. What are you dreaming about?"
+WorkQuestion[22] = "Your entire department is coming to your house for dinner. What are you cooking? (No takeout!)"
+WorkQuestion[23] = "How much do you use LinkedIn?"
+WorkQuestion[24] = "What coworker, past or present, have you learned the most from?"
+WorkQuestion[25] = "What is something at work that immediately puts you in a bad mood?"
+WorkQuestion[26] = "What is a non-monetary reward at work that makes you feel good?"
+WorkQuestion[27] = "What energizes you at work?"
+
+
+
+
+
+
+const PosQuestion = [];
+
+PosQuestion[0] = "What is a small thing in your life that you are grateful for?"
+PosQuestion[1] = "Who has really inspired you?"
+PosQuestion[2] = "What do you consider good self-care?"
+PosQuestion[3] = "What is your comfort food?"
+PosQuestion[4] = "How would your friends describe you?"
+PosQuestion[5] = "What is something that feels cozy to you?"
+PosQuestion[6] = "What is your best physical feature?"
+PosQuestion[7] = "What is something that makes you feel good about humanity?"
+PosQuestion[8] = "What is a random act of kindness that you have experienced?"
+PosQuestion[9] = "What is a good interaction you've had with a stranger that you'll never forget?"
+PosQuestion[10] = "Who makes you laugh?"
+PosQuestion[11] = "What is a quality that makes you like someone immediately?"
+PosQuestion[12] = "What is the most bad-ass thing you've ever done?"
+PosQuestion[13] = "What is the last thing you learned?"
+PosQuestion[14] = "What is a piece of art or media that always makes you feel good?"
+PosQuestion[15] = "What is something someone has done for you, and you never got to thank them?"
+PosQuestion[16] = "What is a company or organization that does good work?"
+PosQuestion[17] = "What makes you feel safe?"
+PosQuestion[18] = "What is something you are genuinely good at?"
+PosQuestion[19] = "What is a memorable micro-interaction you've had with a stranger?"
+PosQuestion[20] = "What do you contribute to the world?"
