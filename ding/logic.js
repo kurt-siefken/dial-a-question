db93bcd5db55f5c6be23f4b593974e17d9be5373
kurt-siefken <109696124+kurt-siefken@users.noreply.github.com>
2026-05-06 16:40:44 -0400

    all

--- ding/logic.js
+++ ding/logic.js
@@ -176,37 +176,3 @@ function updateCountdownStyle() {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-//---------------- HAMBURGER MENU -----------------------//
-
-const menuToggle = document.querySelector(".menu-toggle");
-const closeMenu = document.querySelector(".close-menu");
-const sideMenu = document.querySelector("#sideMenu");
-const overlay = document.querySelector("#overlay");
-
-function openMenu() {
-  sideMenu.classList.add("open");
-  overlay.classList.add("open");
-}
-
-function closeSideMenu() {
-  sideMenu.classList.remove("open");
-  overlay.classList.remove("open");
-}
-
-menuToggle.addEventListener("click", openMenu);
-closeMenu.addEventListener("click", closeSideMenu);
-overlay.addEventListener("click", closeSideMenu);
-
-//---------------- HAMBURGER MENU -----------------------//
-
